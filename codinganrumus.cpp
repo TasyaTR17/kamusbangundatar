318d218f7414ab040b7f9fbaf98d0f6e898bfb69
TasyaTR17 <98446901+TasyaTR17@users.noreply.github.com>
2022-08-08 20:52:37 +0700

    update algoritma

--- codinganrumus.cpp
+++ codinganrumus.cpp
@@ -6,27 +6,43 @@ using namespace std;
 
 int main() {
 
-    float alas, tinggi, jari_jari, luas;
-    char tipe;
+    float alas, tinggi, jari_jari;
+    char tipe, lanjut;
+    //kalau lanjut = 1, maka looping do. Tetapi kalau lanjut = 0, maka program berhenti//
 
-    cout << "Berikut ini adalah program untuk menghitung operasi bangun ruang. Tekan enter untuk memulai";
-    cout << "Tekan \n1 untuk menghitung luas segitiga atau tekan \n2 untuk menghitung luas lingkaran";
-    cin >> tipe;
+    do
+    {
+        cout << "Berikut ini adalah program untuk menghitung operasi bangun ruang. \n Tekan enter untuk memulai";
+        cout << "\n Tekan 1 untuk menghitung luas segitiga atau \n tekan 2 untuk menghitung luas lingkaran";
+        cin >> tipe;
 
-     switch(tipe) {
-        case '1':
-            cout << alas << " * " << tinggi << " * " << " 0.5 " << " = " << alas*tinggi*0.5;
-            break;
+        switch(tipe) {
+            case '1':
+                cout << "\n Masukan Alas: ";
+                cin >> alas;
+                cout << "\n Masukan Tinggi: ";
+                cin >> tinggi;
+                cout << alas << " * " << tinggi << " * " << " 0.5 " << " = " << alas*tinggi*0.5;
+                break;
 
-        case '2':
-            cout << 3.14 << " * " << jari_jari << jari_jari << " = " << 3.14*jari_jari*jari_jari;
-            break;
+            case '2':
+                cout << "\n Masukan jari-jari: ";
+                cin >> jari_jari;
+                cout << 3.14 << " * " << jari_jari << " * " << jari_jari << " = " << 3.14*jari_jari*jari_jari;
+                break;
         
-        default:
-            cout << "Error! operator is undefine";
-            break;
-     }
+            default:
+                cout << "Error! opsi tidak ditemukan";
+                break;
+        }
+
+        cout << "\n Tekan 1 untuk lanjut lagi atau tekan 0 untuk berhenti";
+        cin >> lanjut;
+
+    } 
     
+    while (lanjut == '1');
+   
     return 0;
 
 }
